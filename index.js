1dfc3c0652720e8693a9efe775e371dd888046df
Luke Edwards <luke.edwards05@gmail.com>
2017-03-19 22:30:25 -0700

    Format "Downloaded" message (#10)
    * consolidate sync's piper
    
    * reconstruct 'downloaded' message
    
    * update eslint rules
    
    * rename REPO -> TITLE
    
    * rewrite success logger for legibility
    
    * destructured requires from package.json

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 #!/usr/bin/env node
 
 const prog = require('caporal');
-const pkg = require('./package');
+const { version } = require('./package');
 const { downloadRepo } = require('./lib/repository');
 const { getLocalDependencies } = require('./lib/local');
-const { FROM, ERROR, OK } = require('./lib/constants');
+const { ERROR, OK, TITLE, SPACER } = require('./lib/constants');
 
 function init(repo, logger) {
-    downloadRepo(repo).then((files) => {
-        files.forEach(file => logger.info(OK, `${file} ${FROM} ${repo}`));
-    }).catch(err => logger.error(ERROR, err));
+    downloadRepo(repo)
+        .then((arr) => arr.map((file) => `${SPACER} - ${file}`))
+        .then((arr) => arr.concat('').join('\n'))
+        .then((str) => {
+            logger.info(OK, TITLE(repo));
+            logger.info(str);
+        })
+        .catch((err) => logger.error(ERROR, err));
 }
 
 function initLocal(logger) {
     getLocalDependencies().then((deps) => {
-        deps.forEach(repo => init(repo, logger));
-    }).catch(err => logger.error(ERROR, err));
+        deps.forEach((repo) => init(repo, logger));
+    }).catch((err) => logger.error(ERROR, err));
 }
 
 prog
-    .version(pkg.version)
+    .version(version)
     .argument('[query]', 'Specify the repository to fetch.')
     .action((args, options, logger) => {
+        logger.info('\r'); // padding
+
         if (args.query) {
             return init(args.query, logger);
         }
