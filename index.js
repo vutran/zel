f3e421982e6f52caa3048ecdf4bd409dc58f2603
Vu Tran <vutran@dropbox.com>
2017-03-21 17:29:49 -0700

    fix log constants

--- index.js
+++ index.js
@@ -5,30 +5,30 @@ const prog = require('caporal');
 const { version } = require('./package');
 const { downloadRepo } = require('./lib/repository');
 const { getLocalDependencies } = require('./lib/local');
-const { ERROR, DOWNLOADED, TITLE, SPACER, VALID, INVALID } = require('./lib/constants');
+const { LOG } = require('./lib/constants');
 const Resolver = require('./lib/resolver');
 
 function init(repo, logger) {
     downloadRepo(repo)
-        .then((arr) => arr.map((file) => `${SPACER} - ${file}`))
+        .then((arr) => arr.map((file) => `${LOG.SPACER} - ${file}`))
         .then((arr) => arr.concat('').join('\n'))
         .then((str) => {
-            logger.info(DOWNLOADED, TITLE(repo));
+            logger.info(LOG.DOWNLOADED, LOG.TITLE(repo));
             logger.info(str);
         })
-        .catch((err) => logger.error(ERROR, err));
+        .catch((err) => logger.error(LOG.ERROR, err));
 }
 
 function initLocal(logger) {
     getLocalDependencies()
         .then((deps) => {
             if (!deps.length) {
-                logger.error(ERROR, 'No local dependencies defined. Please define a repository.');
+                logger.error(LOG.ERROR, 'No local dependencies defined. Please define a repository.');
                 return;
             }
             deps.forEach((repo) => init(repo, logger));
         })
-        .catch((err) => logger.error(ERROR, err));
+        .catch((err) => logger.error(LOG.ERROR, err));
 }
 
 prog
@@ -51,12 +51,12 @@ prog
         getLocalDependencies()
             .then((deps) => {
                 new Resolver(options)
-                    .on('valid', (repoName) => logger.info(VALID, repoName))
-                    .on('invalid', (repoName) => logger.error(INVALID, repoName))
-                    .on('error', (err) => logger.error(ERROR, err))
+                    .on('valid', (repoName) => logger.info(LOG.VALID, repoName))
+                    .on('invalid', (repoName) => logger.error(LOG.INVALID, repoName))
+                    .on('error', (err) => logger.error(LOG.ERROR, err))
                     .validate(deps);
             })
-            .catch((err) => logger.error(ERROR, err));
+            .catch((err) => logger.error(LOG.ERROR, err));
     });
 
 prog.parse(process.argv);
