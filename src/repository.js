09b27a32aad849af02533557fc3645124bde29e4
Vu Tran <vutran@dropbox.com>
2017-04-08 19:22:27 -0700

    missed object

--- src/repository.js
+++ src/repository.js
@@ -1,5 +1,7 @@
 // @flow
 
+import type { ZelConfig } from './config';
+import type { ResolvedZelConfig } from './resolvers/base';
 import { join } from 'path';
 import { ZEL } from './constants';
 import { bufferToJSON, get, sync } from './utils';
@@ -9,13 +11,13 @@ import { bufferToJSON, get, sync } from './utils';
  * and creates them in the current directory.
  *
  * @param {string} - The repository name
- * @param {Object} - The zel config object
- * @return {Object} - An object with the repository name and config
+ * @param {ZelConfig} - The zel config object
+ * @return {ZelConfig} - An object with the repository name and config
  */
-export function fetchFiles(repoName: string, config: Object) {
+export function fetchFiles(repoName: string, config: ZelConfig): ResolvedZelConfig {
     if (config.files && config.files.length) {
         config.files.forEach(file => sync(repoName, 'master', file));
     }
 
-    return { repoName, config };
+    return ({ repoName, config }: ResolvedZelConfig);
 }
