997df20facf95bd92630aa08bffe9ccd757ef69d
Luke Edwards <luke.edwards05@gmail.com>
2017-04-14 18:39:47 -0700

    rewrite utils (async)

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,18 @@
 // @flow
 
 import type { ZelConfig } from './types';
-import fs from 'fs';
-import url from 'url';
-import path from 'path';
-import https from 'https';
-import mkdir from 'mkdirp';
+import { dirname, normalize } from 'path';
+import { readFile, writeFile } from 'fs';
 import Promise from 'bluebird';
+import fetch from 'node-fetch';
+import mkdir from 'mkdirp';
 import { name } from '../package';
 
 const mkdirp = Promise.promisify(mkdir);
-const writeFile = Promise.promisify(fs.writeFile);
+const writer = Promise.promisify(writeFile);
+const reader = Promise.promisify(readFile);
+
+const headers = { 'User-Agent': name };
 
 /**
  * Parse a (`.zel`) file's base64 string as JSON.
@@ -36,59 +38,8 @@ export function bufferToJSON(content: Buffer, filepath: string): Object {
  * @return {Promise<T>}
  */
 export function get<T: any>(uri: string, options: any): Promise<T> {
-    return new Promise((resolve, reject) => {
-        if (!uri) {
-            reject('Invalid URL.');
-            return;
-        }
-
-        const opts = Object.assign(
-            {
-                json: false,
-                token: null,
-            },
-            options
-        );
-
-        const u = url.parse(uri);
-        const reqOptions = {
-            protocol: u.protocol,
-            hostname: u.hostname,
-            path: u.path,
-            headers: {
-                'User-Agent': name,
-            },
-        };
-
-        if (opts.token) {
-            reqOptions.headers.Authorization = `token ${opts.token}`;
-        }
-
-        const req = https.get(reqOptions, res => {
-            let data = '';
-            res.on('data', d => {
-                data += d;
-            });
-            res.on('end', () => {
-                let d = '';
-                // attempts to parse json data if necessary
-                if (opts.json) {
-                    try {
-                        d = JSON.parse(data);
-                    } catch (err) {
-                        reject(err);
-                        return;
-                    }
-                } else {
-                    d = data;
-                }
-                resolve(d);
-            });
-        });
-        req.on('error', e => {
-            reject(e);
-        });
-    });
+    const opts = Object.assign({ headers }, options);
+    return fetch(uri, opts).then(res => res.json());
 }
 
 /**
@@ -97,20 +48,9 @@ export function get<T: any>(uri: string, options: any): Promise<T> {
  * @param {string} file - The path to the file
  * @return {Promise<ZelConfig>} - The file contents as JSON Object
  */
-export function getConfig(file: string): Promise<ZelConfig> {
-    return new Promise((resolve, reject) => {
-        fs.readFile(file, (err, buf) => {
-            if (err && err.code === 'ENOENT') {
-                return reject(`File does not exist: ${file}`);
-            }
-            try {
-                const data = (bufferToJSON(buf, file): ZelConfig);
-                resolve(data);
-            } catch (msg) {
-                reject(msg);
-            }
-        });
-    });
+export async function getConfig(file: string): Promise<ZelConfig> {
+    const buf = await reader(file); // err && (err.code === 'ENOENT') && reject(`File does not exist: ${file}`);
+    return (bufferToJSON(buf, file): ZelConfig); // reject(msg);
 }
 
 /**
@@ -120,12 +60,13 @@ export function getConfig(file: string): Promise<ZelConfig> {
  * @param {string} file - The full file"s path.
  * @param {string} data - The data to write.
  * @param {Object} opts - See `fs.writeFile`.
+ * @return {Promise<T>}
  */
-export const write = Promise.method((file: string, data: string, opts: any) => {
-    file = path.normalize(file);
-    const dirs = path.dirname(file);
-    return mkdirp(dirs).then(() => writeFile(file, data, opts));
-});
+export async function write(file: string, data: string, opts: any): Promise<T> {
+    file = normalize(file);
+    await mkdirp(dirname(file));
+    return writer(file, data, opts);
+}
 
 /**
  * Downloads the contents from the given repo file path components.
@@ -133,14 +74,10 @@ export const write = Promise.method((file: string, data: string, opts: any) => {
  * @param {string} repo
  * @param {string} branch
  * @param {string} file
+ * @return {Promise<T>}
  */
-export function sync(repo: string, branch: string, file: string) {
+export async function sync(repo: string, branch: string, file: string): Promise<T> {
     const uri = `https://raw.githubusercontent.com/${repo}/${branch}/${file}`;
-    get(uri)
-        .then(data => {
-            write(file, data);
-        })
-        .catch(err => {
-            throw `Trouble while fetching ${repo}/${branch}/${file}.`;
-        });
+    const data = await get(uri); // throw `Trouble while fetching ${repo}/${branch}/${file}.`;
+    return write(file, data);
 }
