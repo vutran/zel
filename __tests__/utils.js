bb75ffb616256d34e95745f77451821644219466
Vu Tran <vu@vu-tran.com>
2017-04-16 12:46:54 -0700

    test write()

--- __tests__/utils.js
+++ __tests__/utils.js
@@ -1,4 +1,6 @@
-import { bufferToJSON, get } from '../src/utils';
+import { bufferToJSON, get, getConfig, write } from '../src/utils';
+
+jest.mock('fs');
 
 describe('utils', () => {
     describe('bufferToJSON', () => {
@@ -54,7 +56,7 @@ describe('utils', () => {
             fetch.__setResponse('THIS IS A REJECTION');
 
             try {
-                const results = await get('https://rejected.com');
+                await get('https://rejected.com');
             } catch (err) {
                 expect(err).toBe('THIS IS A REJECTION');
             }
@@ -62,8 +64,36 @@ describe('utils', () => {
     });
 
     describe('getConfig', () => {
-        it('should get a config file from the filesystem', () => {
-            // TODO
+        const fs = require('fs');
+
+        it('should get a config file from the filesystem', async () => {
+            const fooData = Buffer.from('{"foo":"FOO"}');
+            const barData = Buffer.from('{"bar":"BAR"}');
+
+            fs.__reset();
+            fs.__setFileData('foo.txt', fooData);
+            fs.__setFileData('bar.txt', barData);
+
+            const fooResults = await getConfig('foo.txt');
+            const barResults = await getConfig('bar.txt');
+
+            expect(fs.readFile)
+                .toBeCalled();
+            expect(fooResults)
+                .toEqual({ foo: 'FOO' });
+            expect(barResults)
+                .toEqual({ bar: 'BAR' });
+        });
+
+        it('should throw an error if file does not exist', async () => {
+            fs.__reset();
+            fs.__setError(new Error('Invalid file'));
+
+            try {
+                await getConfig('invalid.txt');
+            } catch (err) {
+                expect(err).toEqual(new Error('Invalid file'));
+            }
         });
     });
 
@@ -75,8 +105,22 @@ describe('utils', () => {
 
 
     describe('write', () => {
-        it('should write content to a file', () => {
-            // TODO
+        const fs = require('fs');
+        const mkdirp = require('mkdirp');
+
+        it('should write content to a file', async () => {
+            fs.__reset();
+
+            await write('test/foo.txt', 'FOO');
+
+            expect(mkdirp)
+                .toBeCalled();
+            expect(mkdirp.__getDirs())
+                .toContain('test');
+            expect(fs.writeFile)
+                .toBeCalled();
+            expect(fs.__getFiles())
+                .toEqual({ 'test/foo.txt': 'FOO' });
         });
     });
 });
